a30d134f773d15008b1d68aea682310e4b16f40a
Tsi Angwafor <108844670+tangwafor@users.noreply.github.com>
2026-01-02 11:01:18 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/nikom-ni-mankon/', // Change this to your repo name!
+  base: '/nikom/', // Change this to your repo name!
 })
